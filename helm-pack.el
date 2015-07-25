de443010d6aee350b1ddc909b7b8d11ad34954f1
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-07-25 19:12:58 +0200

    Add hook function

--- helm-pack.el
+++ helm-pack.el
@@ -35,11 +35,13 @@
   (define-key helm-map (kbd "M-? C-d") 'helm-debug-output)
   (define-key helm-map (kbd "M-? h") 'helm-help)
 
-  (define-key helm-find-files-map (kbd "~") (lambda ()
-                                              (interactive)
-                                              (if (looking-back "/")
-                                                  (insert "~/")
-                                                (call-interactively 'self-insert-command)))))
+  (add-hook 'helm-find-files-after-init-hook
+            (lambda ()
+              (define-key helm-find-files-map (kbd "~") (lambda ()
+                                                          (interactive)
+                                                          (if (looking-back "/")
+                                                              (insert "~/")
+                                                            (call-interactively 'self-insert-command)))))))
 
 (provide 'helm-pack)
 ;;; helm-pack.el ends here
